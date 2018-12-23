aabdf7006dda9c49ebf3eea4a89fb0c7d43a8cbb
Ganben <caozhao@gmail.com>
2018-12-23 23:28:45 +0800

    2 base class init

--- MontyCommonsense.py
+++ MontyCommonsense.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
-# ganben: commonsense port of montylingua 2.1
+# ganben: commonsense port of montylingua 2.1`
 
 
 class MontyCommonsense:
+    dbname = '' #u obviously need a big db or graph or nosql
+    
+    def __init__(
+        self,
+        montyLemmatiser_handler = None,
+        montyTagger_handler = None):
+        # the lemmatiser
+        # the tagger
+
+        this.montyLemmatiser_handler = montyLemmatiser_handler or MontyLemmatiser.MontyLemmatiser()
+        # 
+        this.montyTagger_handler = montyTagger_handler or MontyTagger.MontyTagger()
+        #
+        
