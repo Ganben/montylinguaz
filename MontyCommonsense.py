9806ae608742642bcbd2f9673970f2d266076450
Ganben <caozhao@gmail.com>
2018-12-31 23:42:27 +0800

    bump commonsense main

--- MontyCommonsense.py
+++ MontyCommonsense.py
@@ -5,7 +5,7 @@
 
 class MontyCommonsense:
     dbname = '' #u obviously need a big db or graph or nosql
-    
+    # cssdb.mdf = 
     def __init__(
         self,
         montyLemmatiser_handler = None,
@@ -18,3 +18,25 @@ class MontyCommonsense:
         this.montyTagger_handler = montyTagger_handler or MontyTagger.MontyTagger()
         #
         
+        return
+
+    def cs_verify_tagged(self, tagged):
+        #verify with hostnames_dict, 
+        # in ['VBD', 'VBG'] tags etc.
+        
+        info1=' '.join(map(lambda tagged_cleaned:tagged_cleaned[0]+'/'+tagged_cleaned[1],map(lambda tmps:tmps.split('/'),cmp1)))
+        
+        return info1
+
+    def unpp(self,pp):
+        cmp1 = pp.strip(' ()\n').split()
+
+        return
+
+    def build_cs_selection_db(self):
+
+        return
+    
+    def setitem(self,dict,key,value):
+        # seems useles?
+        dict[key] = value
