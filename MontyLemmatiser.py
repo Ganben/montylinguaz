8774491f9e6d7ce473ce4b0dff3f08e0d590151e
Ganben <caozhao@gmail.com>
2019-01-01 13:41:06 +0800

    bump some, not finished lemmatiser

--- MontyLemmatiser.py
+++ MontyLemmatiser.py
@@ -21,5 +21,47 @@ class MontyLemmatiser:
     irregular_re_any = []
     irregular_re_verb = []
     # irregular can be tf model/db
+    irregular_nouns += [
 
+    ]
+    # additional irregular nouns
+    def __init__(self):
+        #
+        filename_str=[]
+        self.regular_any,self.regular_verb,self.regular_noun,self.irregular_re_any,self.irregular_re_verbs,self.irregular_re_nouns=map(lambda the_tokenizers:map(lambda the_tokenizer_str:[re.compile('^'+the_tokenizer_str[0].lower()+'$')]+the_tokenizer_str[1:],the_tokenizers),filename_str)
+        return
+
+    def lemmatise_untagged_sentence(self,untagged):
+        #
+
+    def lemmatise_tagged_sentence(self,tagged):
+        #
+
+    
+    def lemmatise_word(self,word,pos=""):
+
+        #
+
+    def verify_lemmatiser(self):
+        #
+    
+    def make_verification_dictionary(self):
+        #
+        return
+
+    def fix_case(self, word1, word2):
+        #
+        return
+
+    def _re_match_helper(self, re_kb, word):
+        #
+        return
+
+    def find_irregular_morph(self,word,pos=""):
+        a1=self._re_match_helper
+        groupnames1=self.find_irregular_morph
+        cron_dictr=a1(self.irregular_re_any,word)
+
+        return
+    
     
