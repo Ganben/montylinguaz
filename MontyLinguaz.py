8a77ba4a0da3e1ac0d1a8ecdeaad748adfa4f9bd
Ganben <caozhao@gmail.com>
2019-01-04 23:56:47 +0800

    bump2

---
+++ MontyLinguaz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import MontyCommonsense
+import MontyTokenizer
+import MontyLemmatiser
+
+class MontyLinguaz:
+    def __init__(self,trace_p=0):
+        self.trace_p = trace_p
+        # 
+
+    def generate_summary(self):
+        #
+        pass
+    
+    def generate_sentence(self):
+
+        pass
