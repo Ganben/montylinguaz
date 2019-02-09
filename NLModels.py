e07819b86901e8c8a5dfbd77f1988b6027dc3df7
Ganben <caozhao@gmail.com>
2019-02-09 22:25:34 +0800

    intro verb script n executor n vm

--- NLModels.py
+++ NLModels.py
@@ -24,19 +24,39 @@ class Verbs(WordBase):
     # 
     def __init__(self, id, cat, c):
         super.__init__(self, id, cat, c)
-        self.pms = []
+        self.polys = []
+        self.ad = []
     def polymorh(self, cm, dsc):
-        self.pms.append((cm, dsc))
-
+        self.polys.append((cm, dsc))
+    
+    def add_ad(self, ads):
+        for el in ads:
+            self.ad.append(el)
+        return
+    
     def add_sub(self, sub):
         self.sub = sub
+        return
 
     def add_obj(self, obj):
         self.obj = obj
     
-    def find(self):
+    @property
+    def graph(self):
+        # add a graph obj
         if self.sub is not None and self.obj is not None:
             return self.sub, self.obj
+
+    def execute(self, env_dict):
+        # execute the verb to env vars
+
+        # 1. query verb's operator and script
+
+        # 2. running script(where? in a VM or ? data object?)
+
+        # 3. update env
+
+
 
 class Preps(WordBase):
 
